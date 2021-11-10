7321becaa8f342f5fcca2d7f5243d6e141c7b16e
satya sindhu <movva001@gmail.com>
2021-11-10 16:56:11 -0500

    modified  express_server.js,urls_index.ejs,urls_show.ejs,_header.ejs

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+app.set("view engine", "ejs");
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -39,6 +39,6 @@ app.get("/urls.json", (req, res) => {
     res.render("hello_world", templateVars);
   });
   app.get("/urls/:shortURL", (req, res) => {
-    const templateVars = { shortURL: req.params.shortURL, longURL: /* What goes here? */ };
+    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
     res.render("urls_show", templateVars);
   });
