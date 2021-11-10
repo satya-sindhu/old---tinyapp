90071c16bdf8c3e9aad3c593da4a1940b6b8127f
satya sindhu <movva001@gmail.com>
2021-11-09 22:37:30 -0500

    modified express_server.js

--- express_server.js
+++ express_server.js
@@ -21,3 +21,20 @@ app.get("/urls.json", (req, res) => {
   app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
   });
+  app.get("/set", (req, res) => {
+    const a = 1;
+    res.send(`a = ${a}`);
+   });
+   
+   app.get("/fetch", (req, res) => {
+    res.send(`a = ${a}`);
+   });
+   app.get("/urls", (req, res) => {
+    const templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+  });       
+
+  app.get("/hello", (req, res) => {
+    const templateVars = { greeting: 'Hello World!' };
+    res.render("hello_world", templateVars);
+  });
