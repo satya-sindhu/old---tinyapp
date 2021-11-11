07afda1f94329464db080be02af4b88543a81ac0
satya sindhu <movva001@gmail.com>
2021-11-10 22:31:22 -0500

    modified updated URL shortening

--- express_server.js
+++ express_server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 const urlDatabase = {
@@ -10,14 +12,23 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-
+app.post("/urls", (req, res) => {
+  //console.log(req.body);  // Log the POST request body to the console
+  const {longURL} = req.body;
+  const shortURL = generateRandomString();
+  //console.log(shortURL);
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);         // Respond with 'Ok' (we will replace this)
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
   });
-  
+  app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
   app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
   });
@@ -42,3 +53,15 @@ app.get("/urls.json", (req, res) => {
     const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
     res.render("urls_show", templateVars);
   });
+  app.get("/u/:shortURL", (req, res) => {
+    // const longURL = ...
+    res.redirect(longURL);
+  });
+  function generateRandomString() {
+    let result = ' ';
+    const charactersLength = characters.length;
+    for (let i = 0; i < 6; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
