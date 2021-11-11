0cf42bff218e8f1ca1a82d087a4fc002863d60e6
satya sindhu <movva001@gmail.com>
2021-11-11 18:28:37 -0500

    modified added login/logout through cookies

--- express_server.js
+++ express_server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
 const PORT = 8080; // default port 8080
 const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -15,17 +17,21 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"], };
   res.render("urls_index", templateVars);
 });       
 
@@ -37,6 +43,7 @@ app.post("/urls", (req, res) => {
   urlDatabase[shortURL] = longURL;
   res.redirect(`/urls/${shortURL}`);         // Respond with 'Ok' (we will replace this)
 });
+
 app.post("/urls/:shortURL/delete", (req, res) => {
   const {shortURL} = req.params;
   delete urlDatabase[shortURL];
@@ -45,7 +52,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 
 app.post("/urls/:shortURL/edit", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
-  const templateVars = { shortURL: req.params.shortURL, longURL: longURL };
+  const templateVars = { shortURL: req.params.shortURL, longURL: longURL , username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
@@ -81,7 +88,10 @@ app.post("/login", (req, res) => {
   res.redirect("/urls");
 });
 
-
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+   res.redirect("/urls");
+});
 function generateRandomString() {
   let result = ' ';
   const charactersLength = characters.length;
